ee599df34b7d0bf5a6abef2594740f6e1b91caee
Miranda Chen <chen_yinglan@163.com>
2018-03-21 20:39:32 -0400

    update draft via update local file
    a better draft with general idea

--- draft.c
+++ draft.c
@@ -1,5 +1,8 @@
-// most basic
-// interact through terminal I/O input
+// Yinglan Chen, March 2018
+
+// most basic version: given an array of strength
+// next step: interact through terminal I/O input
+// future plan: graphic interpretation
 
 struct Player
 {
@@ -7,11 +10,45 @@ struct Player
   int y;
   int angle;
   int blood;
-};
+} player;
 
-void eval()
+void eval(cat,dot)
 
+bool hasEnded(cat, dog)
 
+int main(int argc, char const *argv[])
+{
+  // hardcode for easy start
+  catinput = [];
+  doginput = [];
+
+  // init
+  player* cat = malloc(sizeof(Player));
+  if (cat == NULL) exit(-1);  // null check
+  cat->x = 10;
+  cat->y = 0;
+  cat->angle = 45;
+  cat->blood = 100;
+  player* dog = malloc(sizeof(Player));
+  if (dog == NULL) {free(cat);exit(-1);} // null check
+  dog->x = 90;
+  dog->y = 0;
+  dog->angle = 45;
+  dog->blood = 100;
+  // start game
+  while (!hasEnded(cat,dog)){
+    //randomize a wind speed with direction
+    eval(v)
+    update cat and dog
+    compare blood
+  }
+  // game end
+  // print output
+  printf("?wins\n", );
+  return 0;
+}
+
+/*
 instance: cat, dog
 attribution:
 - position (x,y): default, fixed
@@ -34,10 +71,4 @@ logistics:
 animation
 speical 道具： double attack, huge attack, stink bomb
 set different difficult levels 
-# future
-add AI player
-change angle
-change position
-add other values such as 体力,$$, wall height
-other idea to improve:
-- machine learning! let fleabag hit mutt without seeing him
+*/
